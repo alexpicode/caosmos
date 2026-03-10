726ccd655ba3299dd779e8a7c04640648adc3c74
Alexpi <alexpi8@gmail.com>
2026-03-10 23:58:24 +0100

    init

--- src/main/java/com/caosmos/CaosmosApplication.java
+++ src/main/java/com/caosmos/CaosmosApplication.java
@@ -2,8 +2,12 @@ package com.caosmos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = "com.caosmos")
 public class CaosmosApplication {
 
 	public static void main(String[] args) {
